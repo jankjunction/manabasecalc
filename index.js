c09f969e4ad65d3938b2750d55f9d73fd014fc6f
Micah Bell <micahjoshuabell@gmail.com>
2022-04-24 15:54:23 -0400

    Fix bug with double digit quantities

--- index.js
+++ index.js
@@ -4,26 +4,17 @@ function decklistImport(deck) {
     decklistArray = deck.split('\n');
 
     for (let i = 0; i < decklistArray.length; i++) {
-        let card = {};
-        let qty = decklistArray[i].slice(0, 1);
-        let cardName = '';
-        console.log(typeof(decklistArray[i]));
+        let qty = decklistArray[i].slice(0, decklistArray[i].indexOf(' '));
+        console.log(qty);
+        let cardName = decklistArray[i].slice((decklistArray[i].indexOf(' ') +1), (decklistArray[i].indexOf('(') - 1));
 
-        // Checks for cards that can have double digit quantities
-        if (decklistArray[i].includes('Plains') ||
-            decklistArray[i].includes('Island') ||
-            decklistArray[i].includes('Swamp') ||
-            decklistArray[i].includes('Mountain') ||
-            decklistArray[i].includes('Forest')) {cardName = decklistArray[i].slice(3, (decklistArray[i].indexOf('(') - 1))} else { 
-            cardName = decklistArray[i].slice(2, (decklistArray[i].indexOf('(') - 1));}
-        
         // Omits lines that have 'Decklist' 'Sideboard' or a '\r'
         if ((qty !== 'D') && (qty !== '\r') && (qty !== 'S')) {
-            card.quantity = qty;
             decklist[cardName] = qty;
         } else {
             continue;
         }
+
     }
     console.log(Object.keys(decklist));
     return decklist;
